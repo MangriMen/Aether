ec1b25f3ccdfa6ec5f3348d88b9dd18d1d6a54c8
MangriMen <49300253+MangriMen@users.noreply.github.com>
2024-09-08 01:33:42 +0700

    feat: setup theme and linters

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-import App from "./App";
+import { render } from 'solid-js/web';
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+import App from './app/app';
+
+render(() => <App />, document.getElementById('root') as HTMLElement);
