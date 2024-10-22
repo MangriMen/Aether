e4e86f8d79d7ea008158e6297b391299b9a9c91c
Igor Lyovkin <49300253+MangriMen@users.noreply.github.com>
2024-10-23 05:55:24 +0700

    feat: add java loading (#7)
    Notifications are now removed after download is complete

--- src-tauri/src/utils/result.rs
+++ src-tauri/src/utils/result.rs
@@ -13,6 +13,14 @@ impl From<Box<dyn Error + 'static>> for AetherLauncherError {
     }
 }
 
+impl From<aether_core::Error> for AetherLauncherError {
+    fn from(value: aether_core::Error) -> Self {
+        AetherLauncherError {
+            message: value.to_string(),
+        }
+    }
+}
+
 impl From<anyhow::Error> for AetherLauncherError {
     fn from(value: anyhow::Error) -> Self {
         AetherLauncherError {
--- src/entities/events/ui/EventCard/EventCard.tsx
+++ src/entities/events/ui/EventCard/EventCard.tsx
@@ -2,6 +2,9 @@ import { Component, createMemo, splitProps } from 'solid-js';
 
 import { Progress } from '@/shared/ui';
 
+// eslint-disable-next-line boundaries/element-types
+import { LoadingBarTypeEnum } from '@/entities/minecraft';
+
 import { EventCardProps } from './types';
 
 export const EventCard: Component<EventCardProps> = (props) => {
@@ -9,12 +12,21 @@ export const EventCard: Component<EventCardProps> = (props) => {
 
   const clampedValue = createMemo(() => local.payload.fraction ?? 1);
 
+  const title = createMemo(() => {
+    switch (local.payload.event.type) {
+      case LoadingBarTypeEnum.JavaDownload:
+        return `Downloading Java ${local.payload.event.version}`;
+      case LoadingBarTypeEnum.MinecraftDownload:
+        return local.payload.event.instance_name;
+    }
+  });
+
   return (
     <div
       class='flex flex-col gap-2 rounded-md border bg-background px-4 py-2'
       {...others}
     >
-      <span class='font-bold'>{local.payload.event.instance_name}</span>
+      <span class='font-bold'>{title()}</span>
       <Progress value={clampedValue()} minValue={0} maxValue={1} />
       <span class='text-muted-foreground'>
         {`${Math.round(clampedValue() * 100)}%`}
