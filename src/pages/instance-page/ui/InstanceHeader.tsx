fa50f7a3de88c7ca51cea73bf04668eeac7cb905
MangriMen <49300253+MangriMen@users.noreply.github.com>
2025-01-25 11:07:41 +0700

    feat: improve instance page header

--- src/pages/instance-page/ui/InstanceHeader.tsx
+++ src/pages/instance-page/ui/InstanceHeader.tsx
@@ -1,27 +1,19 @@
-import MdiClockIcon from '@iconify/icons-mdi/clock';
-import MdiEngineIcon from '@iconify/icons-mdi/engine';
 import MdiFolderIcon from '@iconify/icons-mdi/folder';
 import MdiSettingsIcon from '@iconify/icons-mdi/settings';
-import { Icon } from '@iconify-icon/solid';
 import { useNavigate } from '@solidjs/router';
-import {
-  Component,
-  ComponentProps,
-  createMemo,
-  Show,
-  splitProps,
-} from 'solid-js';
+import { Component, ComponentProps, createMemo, splitProps } from 'solid-js';
 
 import { IconButton } from '@/shared/ui';
 
 import {
-  formatTimePlayedHumanized,
   InstanceImage,
   InstanceActionButton,
   useInstanceActions,
   useRunningInstancesContext,
   Instance,
 } from '@/entities/instance';
+
+import InstanceHeaderInfo from './InstanceHeaderInfo';
 
 export type InstanceHeaderProps = ComponentProps<'div'> & {
   instance: Instance;
@@ -41,40 +33,14 @@ export const InstanceHeader: Component<InstanceHeaderProps> = (props) => {
     openFolder: handleOpenFolder,
   } = useInstanceActions();
 
-  const lastPlayedDate = createMemo(() => {
-    return local.instance?.lastPlayed
-      ? new Date(Date.parse(local.instance?.lastPlayed))
-      : undefined;
-  });
-
   const navigateToSettings = () => {
     navigate('settings');
   };
 
   return (
-    <div class='flex gap-4' {...others}>
+    <div class='flex gap-3' {...others}>
       <InstanceImage src={local.instance.iconPath} />
-      <div class='flex flex-col gap-2'>
-        <span class='text-2xl font-bold'>{local.instance.name}</span>
-        <span
-          class='inline-flex items-center gap-1 capitalize text-muted-foreground'
-          title='Modloader'
-        >
-          <Icon icon={MdiEngineIcon} />
-          {local.instance.loader} {local.instance.loaderVersion}
-        </span>
-        <span class='inline-flex items-center gap-1 text-muted-foreground'>
-          <Icon icon={MdiClockIcon} />
-          <Show when={local.instance.timePlayed} fallback='Never played'>
-            <span
-              class='mt-auto inline-flex items-center gap-1 capitalize'
-              title={`Last played: ${lastPlayedDate()?.toLocaleString()}`}
-            >
-              {formatTimePlayedHumanized(local.instance.timePlayed)}
-            </span>
-          </Show>
-        </span>
-      </div>
+      <InstanceHeaderInfo instance={local.instance} />
       <div class='ml-auto flex items-center gap-2'>
         <InstanceActionButton
           class='w-20 p-2'
