9a9aa45428fd684011d5df13008bec7c541bb1e4
MangriMen <49300253+MangriMen@users.noreply.github.com>
2025-06-15 16:37:26 +0700

    fix: fix default dark theme

--- src/1_app/config/theme.ts
+++ src/1_app/config/theme.ts
@@ -3,7 +3,7 @@ import { type ThemeConfig, type ThemeContextValue } from '@/shared/model';
 export const THEME_ATTRIBUTE = 'data-theme';
 export const TRANSPARENCY_PROPERTY = '--transparency';
 
-export const THEME_STATE_LS_KEY = 'themeState';
+export const THEME_STATE_LS_KEY = 'theme-state';
 
 export const DEFAULT_THEME: ThemeConfig = 'aether-dark';
 export const DEFAULT_LIGHT_THEME = 'light';
@@ -13,6 +13,6 @@ export const DEFAULT_THEME_CONTEXT_VALUE: ThemeContextValue = {
   theme: DEFAULT_THEME,
   rawTheme: DEFAULT_THEME,
   lightTheme: DEFAULT_LIGHT_THEME,
-  darkTheme: DEFAULT_LIGHT_THEME,
+  darkTheme: DEFAULT_DARK_THEME,
   transparency: 1,
 };
