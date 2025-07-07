c5aea3dabed4f38666585937f37f3c9ea05042a9
Igor Lyovkin <49300253+MangriMen@users.noreply.github.com>
2025-07-08 04:32:18 +0700

    feat: make notification tray more visible (#75)
    * Renamed notification tray to progress
    * Changed icon from chevron to animated progress bar
    * Changed style of progress popover
    * Added global dialog state
    * Added Progress Details component

---
+++ src/3_widgets/app-titlebar/lib/useProgressMenuShowActions.ts
@@ -0,0 +1,19 @@
+import { createEffect, type Accessor } from 'solid-js';
+
+export const useProgressMenuShowActions = (
+  payloadsCount: Accessor<number>,
+  onZeroCount: () => void,
+  onPrevWasZeroCount: () => void,
+) => {
+  createEffect((prevCount) => {
+    const currCount = payloadsCount();
+
+    if (!currCount) {
+      onZeroCount();
+    } else if (prevCount === 0 && currCount) {
+      onPrevWasZeroCount();
+    }
+
+    return currCount;
+  }, 0);
+};
--- src/5_entities/events/lib/index.ts
+++ src/5_entities/events/lib/index.ts
@@ -1,1 +1,2 @@
 export * from './useWarningEventsListener';
+export * from './useProgressEventsListener';
