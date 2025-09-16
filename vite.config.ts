2b50731f11395aefe3875c1d65e121290f64b827
MangriMen <49300253+MangriMen@users.noreply.github.com>
2025-09-17 05:07:26 +0700

    fix: fix build warnings (recreate lock file)

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,10 @@ export default defineConfig(async () => ({
     tsconfigPaths(),
   ],
 
+  build: {
+    chunkSizeWarningLimit: 1500,
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
