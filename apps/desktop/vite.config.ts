2fbd958426b4a336cc8deeea5dc3d050f05919c7
MangriMen <49300253+MangriMen@users.noreply.github.com>
2026-04-09 03:15:19 +0700

    fix: fix frontend tests

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,38 +1,11 @@
 import type { UserConfig } from 'vite';
 
+import path from 'path';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
 import vitePluginChecker from 'vite-plugin-checker';
 import solid from 'vite-plugin-solid';
 import solidSvg from 'vite-plugin-solid-svg';
-
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  resolve: {
-    tsconfigPaths: true,
-  },
-  plugins: [
-    vitePluginCheckerConfig,
-    solid({
-      hot: process.env.NODE_ENV !== 'test',
-    }),
-    iconsConfig,
-    solidSvg(),
-  ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    transformMode: {
-      web: [/\.[jt]sx?$/],
-    },
-    // isolates context for solid-js
-    deps: {
-      registerNodeLoader: false,
-    },
-  },
-  build: buildConfig,
-  ...developmentConfig,
-}));
 
 const vitePluginCheckerConfig = vitePluginChecker({
   typescript: true,
@@ -41,13 +14,42 @@ const vitePluginCheckerConfig = vitePluginChecker({
   },
 });
 
+const solidPluginConfig = solid({
+  hot: process.env.NODE_ENV !== 'test',
+});
+
 const iconsConfig = Icons({
   compiler: 'solid',
   autoInstall: false,
 });
 
-const buildConfig: UserConfig['build'] = {
-  chunkSizeWarningLimit: 1500,
+const plugins = [
+  vitePluginCheckerConfig,
+  solidPluginConfig,
+  iconsConfig,
+  solidSvg(),
+];
+
+const srcAliases = {
+  '@/app': path.resolve(__dirname, './src/1_app'),
+  '@/pages': path.resolve(__dirname, './src/2_pages'),
+  '@/widgets': path.resolve(__dirname, './src/3_widgets'),
+  '@/features': path.resolve(__dirname, './src/4_features'),
+  '@/entities': path.resolve(__dirname, './src/5_entities'),
+  '@/shared': path.resolve(__dirname, './src/6_shared'),
+};
+
+const testConfig = {
+  environment: 'jsdom',
+  globals: true,
+  transformMode: {
+    web: [/\.[jt]sx?$/],
+  },
+  // isolates context for solid-js
+  deps: {
+    registerNodeLoader: false,
+  },
+  alias: srcAliases,
 };
 
 const developmentConfig: UserConfig = {
@@ -65,3 +67,19 @@ const developmentConfig: UserConfig = {
     },
   },
 };
+
+const buildConfig: UserConfig['build'] = {
+  chunkSizeWarningLimit: 1500,
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  resolve: { tsconfigPaths: true },
+
+  plugins: plugins,
+
+  test: testConfig,
+  build: buildConfig,
+
+  ...developmentConfig,
+}));
